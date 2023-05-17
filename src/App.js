a001da6c905b1e992e87934b6dc26c0c99551657
hj-yuki <hjystfc07gd2020@gmail.com>
2023-05-17 12:38:12 +0900

    mod

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Hello</h2>
+        <h2>Hello!!!</h2>
       </header>
     </div>
   );
